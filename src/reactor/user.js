27352ff58003892af60944ae85116665b9a93873
hasan <hassanzohdy@gmail.com>
2020-05-09 01:14:08 +0200

    Added User Class

---
+++ src/reactor/user.js
@@ -0,0 +1,30 @@
+import cache from 'reactor/cache';
+
+class User {
+    constructor() {
+        this.userData = cache.get('user');
+    }
+
+    /**
+     * Check if user is logged in
+     * 
+     * @returns {boolean}
+     */
+    isLoggedIn() {
+        return this.userData !== null;
+    }
+
+    /**
+     * Log the user in 
+     * It will store the data in the storage engine i.e Local Storage
+     * But will not make the ajax request
+     * 
+     * @param  {object} userData 
+     * @returns {void}
+     */
+    login(userData) {
+        cache.set('user', userData);
+    }
+}
+
+export default new User();
