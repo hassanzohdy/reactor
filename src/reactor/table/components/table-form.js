c9f5e8d223d42021a97cf5146a5f25510025896d
hassanzohdy <hassanzohdy@gmail.com>
2020-07-12 00:09:14 +0200

    Creating Lazy Form

--- src/reactor/table/components/table-form.js
+++ src/reactor/table/components/table-form.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import FormModal from 'reactor/form/components/form-modal';
 import { trans } from 'reactor/localization';
+import ProgressBar from 'reactor/components/progress-bar';
 
 export default function TableForm({ action, formOptions, onClose, itemType, recordIndex, open, onSubmit, service, record }) {
+    const [lazyRecord, setLazyRecord] = React.useState({});
+
+    React.useEffect(() => {
+        if (open && formOptions.lazyForm && record.id && !lazyRecord.id) {
+            let request = formOptions.request || service.get.bind(service);
+
+            request(record.id).then(({ data }) => {
+                setLazyRecord(data.record);
+            });
+        }
+    }, [formOptions, lazyRecord, record, service, open]);
+
     if (!open) return '';
 
     const submitForm = async (e) => {
@@ -25,14 +38,33 @@ export default function TableForm({ action, formOptions, onClose, itemType, reco
         onSubmit && onSubmit(action, savedRecord);
     };
 
+    let content;
+
+    if (formOptions.lazyForm) {
+        if (!lazyRecord.id && record.id) {
+            content = <div style={{ marginTop: '5rem', marginBottom: '5rem' }}>
+                <ProgressBar />
+            </div>
+        } else {
+            content = <formOptions.lazyForm index={recordIndex} record={lazyRecord} />;
+        }
+    } else {
+        content = <formOptions.form index={recordIndex} record={record} />;
+    }
+
+    const onModalClose = () => {
+        setLazyRecord({}); // reset object
+        onClose();
+    }
+
     return (
         <FormModal
             open={open}
             onSubmit={submitForm}
             title={trans(itemType, trans(formOptions.singleName))}
-            onClose={onClose}
+            onClose={onModalClose}
         >
-            <formOptions.lazyForm index={recordIndex} record={record} />
+            {content}
         </FormModal>
     );
 }
