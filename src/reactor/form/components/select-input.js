d0a24f9f0991a0f31a117a5a3a7e47af91fa018c
hassanzohdy <hassanzohdy@gmail.com>
2020-07-15 13:44:50 +0200

    Required Select Input

--- src/reactor/form/components/select-input.js
+++ src/reactor/form/components/select-input.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Label from './label';
 import { Random } from 'reinforcements';
 import { getItem } from '../utils/select-items';
-import { Input, Select } from '@material-ui/core';
+import { Input, Select, FormHelperText } from '@material-ui/core';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import { selectItems, RenderSelectedValues } from './select-input-helper-components';
+import useRequiredInputValidator from '../hooks/use-required-input-validator';
 
 export default function SelectInput({ id, label, onChange, lazyLoading, request, mapResponse, labelId, placeholder, required, value, items, groups, imagable, iconable, multiple, readOnly, none, ...otherProps }) {
     // for multiple selections
-    if (multiple && ! value) {
+    if (multiple && !value) {
         value = [];
     }
 
@@ -23,11 +24,11 @@ export default function SelectInput({ id, label, onChange, lazyLoading, request,
     const [loaded, requestIsLoaded] = React.useState(false);
 
     React.useEffect(() => {
-        if (! lazyLoading || loaded) return;
+        if (!lazyLoading || loaded) return;
 
         request().then(response => {
             const items = mapResponse(response);
-            
+
             setItems(items);
             requestIsLoaded(true);
             setLoading(false);
@@ -36,7 +37,7 @@ export default function SelectInput({ id, label, onChange, lazyLoading, request,
 
     }, [lazyLoading, loaded, request, mapResponse, value]);
 
-    if (none && ! noneAdded) {
+    if (none && !noneAdded) {
         // add none 
         items.unshift({
             value: '',
@@ -46,7 +47,7 @@ export default function SelectInput({ id, label, onChange, lazyLoading, request,
         markNoneAsAdded(true);
     }
 
-    if (placeholder && ! placeholderAdded) {
+    if (placeholder && !placeholderAdded) {
         items.unshift({
             value: Random.id(3),
             label: placeholder,
@@ -57,22 +58,32 @@ export default function SelectInput({ id, label, onChange, lazyLoading, request,
     }
 
     const [opened, setOpenedStatus] = React.useState(false);
+    const [error, setError] = React.useState(null);
 
     const [currentValue, setValue] = React.useState(isLoading ? '' : value || '');
     // get the item object for the given value
 
+    const componentRef = React.useRef();
+
+    const clearRequiredInput = useRequiredInputValidator(required, componentRef, currentValue, setError);
+    const hasError = Boolean(error);
+
     const handleChange = (event) => {
-        let value = event.target.value;        
+        let value = event.target.value;
         setValue(value);
         // select the item by value
         let item = getItem(currentItems, value);
         // set the item as an argument for the onChange event 
         onChange && onChange(item);
+
+        if (value) {
+            clearRequiredInput();
+        }
     };
 
     return (
-        <FormControl fullWidth>
-            <Label component={InputLabel} id={labelId} label={label} />
+        <FormControl fullWidth error={hasError}>
+            <Label component={InputLabel} required={required} id={labelId} label={label} />
             <Select
                 input={<Input />}
                 id={id}
@@ -83,10 +94,12 @@ export default function SelectInput({ id, label, onChange, lazyLoading, request,
                 multiple={multiple}
                 value={currentValue}
                 onChange={handleChange}
-                renderValue={selected => <RenderSelectedValues opened={opened} placeholder={placeholder} label={label} items={currentItems} selected={selected} /> }
+                renderValue={selected => <RenderSelectedValues opened={opened} placeholder={placeholder} label={label} items={currentItems} selected={selected} />}
                 children={selectItems(currentItems, isLoading)}
                 {...otherProps}
             />
+            <FormHelperText error={hasError}>{error}</FormHelperText>
+
         </FormControl>
     );
 }
